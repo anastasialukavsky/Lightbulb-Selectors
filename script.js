f9f708d3e496bcc5a0f33e9016eb40e616b6e3ff
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-20 00:24:08 -0400

    add evt listener clikc count + class toggler

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Write your code here
+const lightbulb1 = document.querySelector('#lightbulb1');
+const lightbulb2 = document.querySelector('#lightbulb2');
+const lightbulb3 = document.querySelector('#lightbulb3');
+
+const subtitle = document.querySelector('.subtitle');
+
+let count = 0;
+
+lightbulb1.addEventListener('click', handleClick);
+lightbulb2.addEventListener('click', handleClick);
+lightbulb3.addEventListener('click', handleClick);
+
+function handleClick() {
+  count++;
+  subtitle.textContent = `You've clicked the lights ${count} time${
+    count === 1 ? '' : 's'
+  }`;
+  this.classList.toggle('active');
+}
